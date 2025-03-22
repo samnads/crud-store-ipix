6d8003e6094f1290a07994b473ad4137198c9c97
Samnad S <samnad.s@azinova.info>
2025-03-22 20:48:35 +0530

    first commit

---
+++ database/migrations/2025_03_22_150628_create_stores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    use SoftDeletes;
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->string('location',255);
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 11, 8);
+            $table->foreignId('created_by')->constrained('users','id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stores');
+    }
+};
